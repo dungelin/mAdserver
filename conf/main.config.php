28ea5bf9f50fe4a7536f105d011268414114c318
WRLin <guangxiao.wang@gmail.com>
2014-02-20 22:55:11 +0800

    initialize file

---
+++ conf/main.config.php
@@ -0,0 +1,32 @@
+
+;<?php exit; ?>
+;*** DO NOT REMOVE THE LINE ABOVE ***
+;------------------------------------------------------------------------------------------;
+; General mAdserve    Installation Settings                                                      ;
+;------------------------------------------------------------------------------------------;
+
+[madserve]
+installed                           = true
+
+;------------------------------------------------------------------------------------------;
+; Database Settings                                                                        ;
+;------------------------------------------------------------------------------------------;
+
+[database]
+type                                = mysql
+host                                = "localhost"
+socket                              =
+port                                =
+username                            = "root"
+password                            = "1qaz@WSX"
+name                                = "madserver"
+
+[reportingdatabase]
+useseparatereportingdatabase     = false
+type                                = mysql
+host                                =
+socket                              =
+port                                =
+username                            =
+password                            =
+name                                =
