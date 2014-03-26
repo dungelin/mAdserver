1ed74c7876649e417cddad9bb1b816caf0b68e7d
WRLin <guangxiao.wang@gmail.com>
2014-03-26 10:53:03 +0800

    中文il18&页面格式化

--- www/cp/signin.php
+++ www/cp/signin.php
@@ -24,7 +24,7 @@ if (logincheck()){
 
 	<title><?php echo getconfig_var('adserver_name'); ?> - <?php echo __('LOGIN');?></title>
 
-<meta charset="utf-8" />
+    <meta charset="utf-8" />
 	<meta name="description" content="" />
 	<meta name="author" content="" />
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
